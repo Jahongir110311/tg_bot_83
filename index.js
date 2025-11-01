adbb3a00680cb95fb88cf3a24fadbf79cfa95bf2
jahongir <baxtoyorovj994@gmail.com>
2025-11-01 16:22:00 +0500

    done

--- index.js
+++ index.js
@@ -2,25 +2,42 @@ import TelegramBot from "node-telegram-bot-api";
 const TOKEN = "7991261995:AAGm6kv9v3q9EHsOLa2K87cs3R6NgS7iLuw"
 const bot = new TelegramBot(TOKEN, { polling: true })
 
-bot.on("message",function(msg){
-    const chatId = msg.chat.id;
-    const text = msg.text;
-    const firstname = msg.chat.first_name;
+bot.on("message", async function (msg) {
+  const chatId = msg.chat.id;
+  const text = msg.text;
+  const firstname = msg.chat.first_name;
+  const menuPhoto = "./assets/images/2.webp";
 
-
-    bot.sendMessage(chatId,`Xush kelibsiz!, ${firstname}` , {
-        reply_markup: {
-            keyboard: [
-                [{text: "Boshlash 🔥"}],
-                [{text: "Menu 🫡"},{text: "Sozlamalar ⚙️"}]
-            ],
-            resize_keyboard: true,
-        },
+  if (text == "/start") {
+    bot.sendMessage(chatId, `Xush kelibsiz, ${firstname}`, {
+      reply_markup: {
+        keyboard: [
+          [{ text: "Boshlash 🔥" }],
+          [{ text: "Menu 😜" }, { text: "Sozlamalar ⚙️" }],
+        ],
+        resize_keyboard: true,
+      },
     });
+  } else if (text == "Boshlash 🔥") {
+    bot.sendMessage(chatId, "Salom, sizga qanday yordam bera olaman?");
+  } else if (text == "Menu 😜") {
+    const kutingXabari = await bot.sendMessage(chatId, "Iltimos kuting...");
 
+    setTimeout(function () {
+      bot.deleteMessage(chatId, kutingXabari.message_id);
+      bot.sendPhoto(chatId, menuPhoto, {
+        caption: "Bizning menyuyimiz...",
+        reply_markup: {
+          keyboard: [
+            [{ text: "Manti" }, { text: "Karam" }],
+            [{ text: "Shashlik" }, { text: "Hotdog" }],
+          ],
+        },
+      });
+    }, 1000);
+  }
 
-    console.log(msg);
-})
-
+  console.log(msg);
+});
 
 console.log("Bot ishga tushdi...");
